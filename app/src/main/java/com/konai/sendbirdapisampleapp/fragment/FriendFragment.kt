d44291e95de550fdcc1af525b87a583f4e6b1927
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-09-19 14:56:30 +0900

    TODO detail user profile

--- app/src/main/java/com/konai/sendbirdapisampleapp/fragment/FriendFragment.kt
+++ app/src/main/java/com/konai/sendbirdapisampleapp/fragment/FriendFragment.kt
@@ -18,6 +18,7 @@ class FriendFragment : BaseFragment<FragmentFriendBinding>(R.layout.fragment_fri
         super.initView()
 
         initUserProfile()
+
         binding.myChannelButton.setOnClickListener {
             createMyChannel()
         }
